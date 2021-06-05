3fdeb5b7952667b08fd3c4da45623fc20a3ff217
Adnan Khan <adnan@airlifttech.com>
2021-06-06 01:09:26 +0500

    Conflict strategy added for insert.

--- app/src/main/java/com/boonapps/repos/db/RepoDao.kt
+++ app/src/main/java/com/boonapps/repos/db/RepoDao.kt
@@ -2,6 +2,7 @@ package com.boonapps.repos.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.boonapps.repos.models.Repo
 
@@ -11,6 +12,6 @@ interface RepoDao {
     @Query("SELECT * FROM repo")
     fun getAll(): List<Repo>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(users: List<Repo>)
 }
